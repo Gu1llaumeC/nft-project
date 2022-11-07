f8fec139b95e9eb9b82168ccae2f401539b989b2
clementpellen <clementpellen@gmail.com>
2022-11-07 16:36:13 +0100

    creation of collection and header

---
+++ src/organisms/Header/header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledHeader } from "./headerStyled";
+
+const Header = () => {
+    return <StyledHeader>Yolo</StyledHeader>;
+};
+
+export default Header;
---
+++ src/pages/Collection/collection.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+// import { StyledCollection } from "./collectionStyled";
+
+const Collection = () => {
+    return <div>Yolo</div>;
+};
+
+export default Collection;
