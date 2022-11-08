d7b8d4fd7d36f0c86b7abd0d887b9459745b9752
emrichardson <emrichardsonprovence@gmail.com>
2022-11-08 22:42:28 +0100

    css login content

---
+++ src/organisms/LoginContent/indexStyled.jsx
@@ -0,0 +1,7 @@
+import style from 'styled-components';
+
+export const StyledLoginContent = style.div`
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 50px 0;`;
