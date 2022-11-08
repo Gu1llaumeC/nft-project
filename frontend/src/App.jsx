46d06c3a73f14d4b8ba42f60f9bd387efc5edf92
clementpellen <clementpellen@gmail.com>
2022-11-08 18:46:32 +0100

    market page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
+import Market from "./pages/Market";
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Collection" element={<Collection />} />
-          <Route path="Market" element={<Home />} />
+          <Route path="Market" element={<Market />} />
         </Routes>
       </BrowserRouter>
     </div>
