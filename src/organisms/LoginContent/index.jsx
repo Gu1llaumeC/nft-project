c1f24fcc42674a525e7209927e78d07c599eaf31
emrichardson <emrichardsonprovence@gmail.com>
2022-11-08 22:42:20 +0100

    create login content

---
+++ src/organisms/LoginContent/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledLoginContent } from "./indexStyled";
+
+import LoginButton from "../../atoms/MintButton";
+
+const LoginContent = () => {
+    return <StyledHeader>
+        <LoginButton />
+    </StyledHeader>;
+};
+
+export default Header;
