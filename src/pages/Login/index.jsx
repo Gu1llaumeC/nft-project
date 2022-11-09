df414b89c6ce52f03e3f2fc757cfd1a2ac2fbecc
emrichardson <emrichardsonprovence@gmail.com>
2022-11-09 08:15:38 +0100

    Improve login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,46 @@
 import React from "react";
+import Web3 from "web3";
+import { useState } from "react";
 import { StyledLogin } from "./indexStyled";
 
-import LoginContent from "../../organisms/LonginContent";
+import LoginButton from "../../atoms/LonginButton";
 
 const Login = () => {
+    
+    const [loading, setLoading] = useState(false);
+    const [address, setAddress] = useState("");
+
+    const onPressConnect = async () => {
+        setLoading(true);
+
+        try {
+            if (window?.ethereum?.isMetaMask) {
+            // Desktop browser
+            const accounts = await window.ethereum.request({
+            method: "eth_requestAccounts",
+            });
+
+            const account = Web3.utils.toChecksumAddress(accounts[0]);
+            setAddress(account);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        setLoading(false);
+    };
+
+    const onPressLogout = () => setAddress("");
+    
     return <StyledLogin>
         <h1>Welcome on our NFT Project</h1>
         <h2>Please Login with Metamask</h2>
-        <LoginContent />
+        <LoginButton
+          onPressConnect={onPressConnect}
+          onPressLogout={onPressLogout}
+          loading={loading}
+          address={address}
+        />
     </StyledLogin>;
 };
 
