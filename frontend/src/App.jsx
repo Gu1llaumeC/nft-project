d97f4b228e215291fd3e22188006147bc54f4089
emrichardson <emrichardsonprovence@gmail.com>
2022-11-09 08:17:27 +0100

    Add route login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Collection from "./pages/Collection";
 import Market from "./pages/Market";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Collection" element={<Collection />} />
-          <Route path="Market" element={<Market />} />
+          <Route path="/Market" element={<Market />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
