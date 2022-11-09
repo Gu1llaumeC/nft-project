c5d2e4401da385b2bf2d10e8fd9823ece38b7969
Gu1llaumeC <90322869+Gu1llaumeC@users.noreply.github.com>
2022-11-09 23:04:01 +0100

    Gu1llaume c/cleaning (#8)
    * FEATURE: cleaned
    
    * FEATURE: cleaned

--- frontend/src/pages/Collection/index.jsx
+++ frontend/src/pages/Collection/index.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { StyledCollection } from "./indexStyled";
-
+import Web3 from "web3";
 import Header from "../../organisms/Header";
 import Content from "../../organisms/Content";
 
 const Collection = () => {
+  const [address, setAddress] = useState(null);
+
+  async function fetchAddress() {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    return accounts;
+  }
+
+  useEffect(() => {
+    if (window?.ethereum?.isMetaMask) {
+      // Desktop browser
+      const accounts = fetchAddress();
+      const account = Web3.utils.toChecksumAddress(accounts[0]);
+      setAddress(account);
+    }
+  });
   return (
     <StyledCollection>
       <Header />
