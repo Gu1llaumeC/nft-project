dbd916e1a92cdf82b701d31df0c4848b4e46030b
emrichardson <emrichardsonprovence@gmail.com>
2022-11-09 09:17:22 +0100

    Improve login button missing the style on the button

--- src/atoms/LoginButton/index.jsx
+++ src/atoms/LoginButton/index.jsx
@@ -1,32 +1,32 @@
 import React from "react";
-import { StyledLoginButton } from "./indexStyled";
+import styles from "./LoginButton.css";
 
 const LoginButton = ({
-  onPressLogout,
-  onPressConnect,
-  loading,
-  address,
-}) => {
-  return (
-    <StyledLoginButton>
-      {address && !loading ? (
-        <button onClick={onPressLogout} className={styles["connect-wallet"]}>
-          Disconnect
-        </button>
-      ) : loading ? (
-        <button
-          className={`${styles["connect-wallet"]} ${styles["connect-button-loading"]}`}
-          disabled
-        >
-          <div>Loading...</div>
-        </button>
-      ) : (
-        <button onClick={onPressConnect} className={styles["connect-wallet"]}>
-          Connect Wallet
-        </button>
-      )}
-    </StyledLoginButton>
-  );
+    onPressLogout,
+    onPressConnect,
+    loading,
+    address,
+  }) => {
+    return (
+      <div>
+        {address && !loading ? (
+          <button onClick={onPressLogout} className={styles["connect-wallet"]}>
+            Disconnect
+          </button>
+        ) : loading ? (
+          <button
+            className={`${styles["connect-wallet"]} ${styles["connect-button-loading"]}`}
+            disabled
+          >
+            <div>Loading...</div>
+          </button>
+        ) : (
+          <button onClick={onPressConnect} className={styles["connect-wallet"]}>
+            Connect Wallet
+          </button>
+        )}
+      </div>
+    );
 };
 
 export default LoginButton;
