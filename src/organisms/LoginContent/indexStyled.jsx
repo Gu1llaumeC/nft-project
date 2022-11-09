709d4c757b5b3fbf9149174717a1dc0f56411b93
emrichardson <emrichardsonprovence@gmail.com>
2022-11-09 08:15:29 +0100

    Get rid off useless files

--- src/organisms/LoginContent/indexStyled.jsx
+++
@@ -1,7 +0,0 @@
-import style from 'styled-components';
-
-export const StyledLoginContent = style.div`
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 50px 0;`;
